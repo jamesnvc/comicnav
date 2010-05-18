109e2687e958ecfb9fb7c173dd4a3388b02a8371
James N. V. Cash <james.nvc@gmail.com>
2010-05-17 21:54:09 -0400

    Getting extension working

--- gotoPrevNext.js
+++ gotoPrevNext.js
@@ -1,54 +1,63 @@
-function click_xpath(xpath_str) {
+var xpath_obj = { };
+var recieved = false;
+
+function clickXPath(xpath_str) {
   var e = new XPathEvaluator();
   var r = e.evaluate(xpath_str,
     document.documentElement, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null);
-  r.singleNodeValue.click(); // Use '.click' instead?
+  window.location = r.singleNodeValue.href;
 }
 
-function trimmed_uri (uri) {
-  var copy = new String(uri);
-  return copy.replace(/^http:\/\/[^/]*(\/.*$)/, "");
-}
-
-function get_xpath() {
+function getXPath() {
   var uri_root = window.location.hostname;
-  var res;
+  chrome.extension.onRequest.addListener(
+    function(request, sender, sendResp) {
+      console.log("Got response ");
+      console.log(request);
+      if (request.name == "gotPageXPath") {
+        xpath_obj = request.xpath;
+        recieved = true;
+        sendResp({});
+        maybeInstall();
+      }
+    });
+  console.log("Added listener");
   chrome.extension.sendRequest({name: "getPageXPath", hostname: uri_root},
-                               function(response) {
-                                 console.log("Response:");
-                                 console.log(response);
-                                 res = response;
-                               });
-  console.log("Res:");
-  console.log(res);
-  return res;
+                               function (response){ console.log("Callback");});
+  console.log("Sent request");
 }
 
-function goto_prev() {
-  var xpath = get_xpath()["prev"];
-  click_xpath(xpath);
+function gotoPrev() {
+  var xpath = xpath_obj.prev;
+  clickXPath(xpath);
 }
 
-function goto_next() {
-  var xpath = get_xpath()["next"];
-  click_xpath(xpath);
+function gotoNext() {
+  var xpath = xpath_obj.next;
+  clickXPath(xpath);
 }
 
-function setup() {
-  console.log("Checking for " + trimmed_uri(window.location.hostname));
-  if (get_xpath() != undefined) {
+function maybeInstall () {
+  if (xpath_obj != undefined &&
+      xpath_obj.prev != undefined &&
+      xpath_obj.next != undefined) {
     console.log("Adding keybindings for this site!");
-    $('document').keyup(function (event) {
+    $(document).keyup(function (event) {
       console.log("Key pressed");
       if (event.keyCode == '39') {
         event.preventDefault();
-        goto_next();
+        gotoNext();
       } else if (event.keyCode == '37') {
         event.preventDefault();
-        goto_prev();
+        gotoPrev();
       }
     });
+    console.log("Installed keyup handler");
   };
+}
+
+function setup() {
+  getXPath();
 }
 
 $(document).ready(setup);
