809aadf9ab92e1b816236781037fcfd38c1b8f30
James N. V. Cash <james.nvc@gmail.com>
2010-06-03 12:04:53 -0400

    Adding ability to add shortcuts via popup

--- popup_info.js
+++ popup_info.js
@@ -2,11 +2,20 @@ function getInfo() {
   chrome.tabs.getSelected(null, function(tab) {
     var hostname = parseURL(tab.url).host;
     site_info = localStorage.getItem(hostname);
-    if (site_info) {
+    if (site_info) { // Bindings exist for this site; show them
       xpath_obj = $.parseJSON(site_info);
       html_str  = "<strong>Previous</strong>: <code>" + xpath_obj.prev + "</code></br>";
       html_str += "<strong>Next</strong>: <code>" + xpath_obj.next + "</code>";
       $('#site_bindings').html(html_str);
+    } else { // No bindings exist; set up form to add bindings
+      $('#add_shortcut').submit(function(event) {
+        event.preventDefault();
+        var next_xpath = $('#next').val().replace(/"/g, '\\u0022');
+        var prev_xpath =$('#prev').val().replace(/"/g, '\\u0022');
+        var obj_str = "{\"next\": \"" + next_xpath + "\", \"prev\": \"" + prev_xpath + "\"}";
+        localStorage.setItem(hostname, obj_str);
+        getInfo();
+      })
     }
   });
 }
