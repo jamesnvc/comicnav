00fa4337ea7a8282ac7dc3182388c413eea5514d
James N. V. Cash <james.nvc@gmail.com>
2010-06-02 21:33:19 -0400

    Making popup javascript stuff display bindings for current page if they exist

--- popup_info.js
+++ popup_info.js
@@ -1,11 +1,24 @@
 function getInfo() {
-  var site_info = localStorage.getItem(window.location.hostname);
-  if (site_info) {
-    xpath_obj = $.parseJSON(site_info);
-    html_str  = "<strong>Previous</strong>: " + xpath_obj.prev + "</br>";
-    html_str += "<strong>Next</strong>: " + xpath_obj.next + "</br>";
-    $('#site_bindings').html(html_str);
-  }
+  chrome.tabs.getSelected(null, function(tab) {
+    var hostname = parseURL(tab.url).host;
+    site_info = localStorage.getItem(hostname);
+    if (site_info) {
+      xpath_obj = $.parseJSON(site_info);
+      html_str  = "<strong>Previous</strong>: <code>" + xpath_obj.prev + "</code></br>";
+      html_str += "<strong>Next</strong>: <code>" + xpath_obj.next + "</code>";
+      $('#site_bindings').html(html_str);
+    }
+  });
+}
+
+function parseURL(url) { 
+  var loc      = { 'href' : url };
+  var parts    = url.replace('//', '/').split('/');
+  loc.protocol = parts[0];
+  loc.host     = parts[1];
+  parts[1]     = parts[1].split(':');
+  loc.hostname = parts[1][0];
+  return loc; 
 }
 
 $(document).ready(getInfo);
