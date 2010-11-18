f0769fe1a63b6e4240e7cb2ac4495ba6e7b7e83e
James N. V. Cash <james.nvc@gmail.com>
2010-11-17 19:51:33 -0500

    Adding ability to edit and delete existing XPath

--- options_setup.js
+++ options_setup.js
@@ -1,20 +1,86 @@
+function editItem (item) {
+  var row = item[0].parentNode.parentNode;
+  var items = $('td', row);
+  var prev_xpath = items[1].innerHTML;
+  var next_xpath = items[2].innerHTML;
+
+  var prev_in = document.createElement('input');
+  prev_in.setAttribute('value', prev_xpath);
+  prev_in.setAttribute('class', 'prev_xpath');
+  $(items[1]).replaceWith(prev_in);
+
+  var next_in = document.createElement('input');
+  next_in.setAttribute('value', next_xpath);
+  next_in.setAttribute('class', 'next_xpath');
+  $(items[2]).replaceWith(next_in);
+
+  $('.editItem', row).removeClass('editItem').addClass('saveItem').text('Save');
+}
+
+function saveItem (item) {
+  var row = item[0].parentNode.parentNode;
+  var hostname = $('td', row)[0].innerHTML;
+  var prev_xpath = $('input.prev_xpath', row).attr('value').replace(
+      /"/g, '\\u0022');
+  var next_xpath = $('input.next_xpath', row).attr('value').replace(
+      /"/g, '\\u0022');
+  var obj_str = "{\"next\": \"" + next_xpath + "\", \"prev\": \"" +
+    prev_xpath + "\"}";
+  localStorage.setItem(hostname, obj_str);
+
+  var prev_td = document.createElement('td');
+  prev_td.appendChild(document.createTextNode(prev_xpath));
+  $('input.prev_xpath', row).replaceWith(prev_td);
+
+  var next_td = document.createElement('td');
+  next_td.appendChild(document.createTextNode(next_xpath));
+  $('input.next_xpath', row).replaceWith(next_td);
+
+  $('.editItem', row).removeClass('editItem').addClass('editItem').text('Edit');
+
+  display_current_sites();
+}
+
+function delItem (item) {
+  var row = item[0].parentNode.parentNode;
+  // Hostname is the first entry in the row
+  var hostname = $('td', row)[0].innerHTML;
+  localStorage.removeItem(hostname);
+  display_current_sites();
+}
+
 function options_init () {
   $('#add_comic_info').submit(function (event) {
     event.preventDefault();
     var uri = trimmed_uri($('#uri').val());
     var next_xpath = $('#next').val().replace(/"/g, '\\u0022');
     var prev_xpath =$('#prev').val().replace(/"/g, '\\u0022');
-    var obj_str = "{\"next\": \"" + next_xpath + "\", \"prev\": \"" + prev_xpath + "\"}";
+    var obj_str = '{"next": "' + next_xpath + '", "prev": "' + prev_xpath + '"}';
     localStorage.setItem(uri, obj_str);
     display_current_sites();
-    }
-  );
+    });
   display_current_sites();
 
   $("#uri").val("xkcd.com");
   $("#prev").val("//div[@class=\"menuCont\"]/ul/li/a[text()=\"< Prev\"]");
   $("#next").val("//div[@class=\"menuCont\"]/ul/li/a[text()=\"Next >\"]");
+  $('.delItem').live('click', function(evt) {
+    evt.preventDefault();
+    var item = $(this);
+    delItem(item);
+  });
+  $('.editItem').live('click', function(evt) {
+    evt.preventDefault();
+    var item = $(this);
+    editItem(item);
+  });
+  $('.saveItem').live('click', function(evt) {
+    evt.preventDefault();
+    var item = $(this);
+    saveItem(item);
+  });
 }
+
 function display_current_sites () {
   var tbl = "";
   for (var elt in localStorage) {
@@ -27,7 +93,9 @@ function display_current_sites () {
       console.log(e);
       return;
     }
-    str += "<td>" + sel["prev"] + "</td><td>" + sel["next"] + "</td></tr>";
+    str += ('<td>' + sel['prev'] + '</td><td>' + sel['next'] + '</td><td>' +
+        '<a href="#" class="delItem">Delete</a>&nbsp;<a href="#" class="editItem">' +
+        'Edit</a></td></tr>');
     tbl += str;
   }
   $('tbody#sites').html(tbl);
